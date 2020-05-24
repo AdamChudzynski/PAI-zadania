b75ab372866795c3297dff09a316c30dc4ac7392
AdamChudzynski <adam.chudzynski@student.put.poznan.pl>
2020-05-24 19:41:59 +0200

    asp .net mvc

---
+++ zad9/asp .net mvc/Models/MusicDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace asp.net_mvc.Models 
+{
+    public class MusicDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Genre> Genres
+        { get; set; }
+    }
+}
---
+++ zad9/asp .net mvc/Models/Song.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace asp.net_mvc.Models
+{
+    public class Song
+    {
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name
+        { get; set; }
+
+        [Required(ErrorMessage = "Artists name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Artist
+        { get; set; }
+
+        public int GenreId
+        { get; set; }
+
+        public int Id
+        { get; set; }
+
+    }
+
+    public class Genre
+    {
+        public int Id
+        { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name
+        { get; set; }
+
+        public ICollection<Song> Songs
+        { get; set; }
+    }
+}
